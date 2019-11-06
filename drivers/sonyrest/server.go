d0b641bfaa1d600e49a7393a34ef4997202da7f2
Andrew Werner <42447312+werndog@users.noreply.github.com>
2019-11-06 09:24:17 -0700

    updating display active signal endpoint (#5)

---
+++ drivers/sonyrest/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/byuoitav/av-control-api/drivers"
+	"github.com/byuoitav/sonyrest-driver"
+	"github.com/spf13/pflag"
+)
+
+func main() {
+	var port int
+	pflag.IntVarP(&port, "port", "p", 80, "port to run the server on")
+
+	pflag.Parse()
+
+	addr := fmt.Sprintf(":%d", port)
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Printf("failed to start server: %s\n", err)
+		os.Exit(1)
+	}
+
+	create := func(ctx context.Context, addr string) (drivers.Display, error) {
+		return &sonyrest.TV{
+			Address: addr,
+			PSK:     "***REMOVED***",
+		}, nil
+	}
+
+	server, err := drivers.CreateDisplayServer(create)
+	if err = server.Serve(lis); err != nil {
+		fmt.Printf("error while listening: %s\n", err)
+		os.Exit(1)
+	}
+
+}
