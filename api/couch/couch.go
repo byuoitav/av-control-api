3aff86003992125e380f7630de523b969c227e6f
Andrew Werner <klinc9@gmail.com>
2020-05-07 17:02:25 -0600

    let's not upload couch credentials

--- api/couch/couch.go
+++ api/couch/couch.go
@@ -1,10 +1,98 @@
 package couch
 
-import "github.com/byuoitav/av-control-api/api"
+import (
+	"errors"
+	"fmt"
+
+	_ "github.com/go-kivik/couchdb/v4"
+	kivik "github.com/go-kivik/kivik/v4"
+	"golang.org/x/net/context"
+)
 
 type DataService struct {
+	DBAddress  string
+	DBUsername string
+	DBPassword string
 }
 
-func (d *DataService) Room(id string) ([]api.Device, error) {
-	return []api.Device{}, nil
+// Room gets a room
+func (d *DataService) Room(ctx context.Context, id string) ([]device, error) {
+	addr := fmt.Sprintf("http://%s:%s@%s", d.DBUsername, d.DBPassword, d.DBAddress)
+	client, err := kivik.New("couch", addr)
+	if err != nil {
+		return []device{}, fmt.Errorf("unable to connect to couch: %s", err)
+	}
+
+	db := client.DB(context.TODO(), "devices")
+
+	roomQuery := kivik.Options{
+		"selector": map[string]interface{}{
+			"_id": map[string]interface{}{
+				"$regex": id + "-.*",
+			},
+		},
+	}
+
+	devices, err := db.Find(context.TODO(), roomQuery)
+	if err != nil {
+		return []device{}, fmt.Errorf("unable to find devices in room %s: %s", id, err)
+	}
+
+	var toReturn []device
+	added := false
+
+	for devices.Next() {
+		if devices.EOQ() {
+			break
+		}
+		var device device
+		err = db.Get(ctx, devices.ID()).ScanDoc(&device)
+		if err != nil {
+			fmt.Printf("error scanning in device doc\n")
+			continue
+		}
+
+		toReturn = append(toReturn, device)
+		added = true
+	}
+
+	if added {
+		return toReturn, nil
+	}
+	return []device{}, errors.New("unable to get room")
+}
+
+// Device gets a device
+func (d *DataService) Device(ctx context.Context, id string) (device, error) {
+	addr := fmt.Sprintf("http://%s:%s@%s", d.DBUsername, d.DBPassword, d.DBAddress)
+	client, err := kivik.New("couch", addr)
+	if err != nil {
+		return device{}, fmt.Errorf("unable to connect to couch: %s", err)
+	}
+
+	db := client.DB(ctx, "devices")
+
+	var device device
+	err = db.Get(ctx, id).ScanDoc(&device)
+	if err != nil {
+		return device, fmt.Errorf("error retrieving device doc: %s", err)
+	}
+
+	return device, nil
+}
+
+// IsHealthy is a healthcheck for the database
+func (d *DataService) IsHealthy(ctx context.Context, dbName string) (bool, error) {
+	addr := fmt.Sprintf("http://%s:%s@%s", d.DBUsername, d.DBPassword, d.DBAddress)
+	client, err := kivik.New("couch", addr)
+	if err != nil {
+		return false, fmt.Errorf("unable to connect to couch: %s", err)
+	}
+
+	alive, err := client.DBExists(ctx, dbName)
+	if err != nil {
+		return false, err
+	}
+
+	return alive, err
 }
---
+++ api/couch/couch_test.go
@@ -0,0 +1,75 @@
+package couch
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"golang.org/x/net/context"
+)
+
+func TestRoom(t *testing.T) {
+	d := DataService{
+		DBAddress:  strings.Trim(os.Getenv("DB_ADDRESS"), "https://"),
+		DBUsername: os.Getenv("DB_USERNAME"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+	}
+
+	devices, err := d.Room(context.TODO(), "ITB-1108A")
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i := range devices {
+		fmt.Printf("ID: %s\n", devices[i].ID)
+		fmt.Printf("Address: %s\n", devices[i].Address)
+		fmt.Printf("Type: %s\n", devices[i].TypeID)
+	}
+
+	devices, err = d.Room(context.TODO(), "ITB-1108B")
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i := range devices {
+		fmt.Printf("ID: %s\n", devices[i].ID)
+		fmt.Printf("Address: %s\n", devices[i].Address)
+		fmt.Printf("Type: %s\n", devices[i].TypeID)
+	}
+}
+
+func TestDevice(t *testing.T) {
+	d := DataService{
+		DBAddress:  strings.Trim(os.Getenv("DB_ADDRESS"), "https://"),
+		DBUsername: os.Getenv("DB_USERNAME"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+	}
+
+	device, err := d.Device(context.TODO(), "ITB-1108A-CP1")
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Printf("ID: %s\n", device.ID)
+	fmt.Printf("Address: %s\n", device.Address)
+	fmt.Printf("Type: %s\n", device.TypeID)
+
+	device, err = d.Device(context.TODO(), "ITB-1108A-CP2")
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Printf("ID: %s\n", device.ID)
+	fmt.Printf("Address: %s\n", device.Address)
+	fmt.Printf("Type: %s\n", device.TypeID)
+
+	device, err = d.Device(context.TODO(), "ITB-1108A-CP3")
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Printf("ID: %s\n", device.ID)
+	fmt.Printf("Address: %s\n", device.Address)
+	fmt.Printf("Type: %s\n", device.TypeID)
+}
