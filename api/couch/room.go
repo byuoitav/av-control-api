4d50a5d722dab3a3cfa59259c770edcd8c163dc0
Daniel Randall <dgrandall@me.com>
2020-06-24 13:32:31 -0600

    move proxying to the room level

--- api/couch/room.go
+++ api/couch/room.go
@@ -2,22 +2,21 @@ package couch
 
 import (
 	"fmt"
-	"regexp"
 
 	"github.com/byuoitav/av-control-api/api"
 	"golang.org/x/net/context"
 )
 
 type room struct {
-	ID      string            `json:"_id"`
-	Devices map[string]device `json:"devices"`
+	ID           string            `json:"_id"`
+	ProxyBaseURL string            `json:"proxyBaseURL"`
+	Devices      map[string]device `json:"devices"`
 }
 
 type device struct {
-	Address string            `json:"address"`
-	Driver  string            `json:"driver"`
-	Proxy   map[string]string `json:"proxy"`
-	Ports   []port            `json:"ports"`
+	Address string `json:"address"`
+	Driver  string `json:"driver"`
+	Ports   []port `json:"ports"`
 }
 
 type port struct {
@@ -39,8 +38,9 @@ func (d *DataService) Room(ctx context.Context, id string) (api.Room, error) {
 
 func (r room) convert() (api.Room, error) {
 	room := api.Room{
-		ID:      r.ID,
-		Devices: make(map[api.DeviceID]api.Device),
+		ID:           r.ID,
+		ProxyBaseURL: r.ProxyBaseURL,
+		Devices:      make(map[api.DeviceID]api.Device),
 	}
 
 	for id, dev := range r.Devices {
@@ -59,20 +59,10 @@ func (d device) convert() (api.Device, error) {
 	dev := api.Device{
 		Address: d.Address,
 		Driver:  d.Driver,
-		Proxy:   make(map[*regexp.Regexp]string),
 	}
 
 	for i := range d.Ports {
 		dev.Ports = append(dev.Ports, d.Ports[i].convert())
-	}
-
-	for k, v := range d.Proxy {
-		regex, err := regexp.Compile(k)
-		if err != nil {
-			return dev, err
-		}
-
-		dev.Proxy[regex] = v
 	}
 
 	return dev, nil
--- api/data.go
+++ api/data.go
@@ -2,8 +2,6 @@ package api
 
 import (
 	"context"
-	"encoding/json"
-	"regexp"
 )
 
 type DataService interface {
@@ -19,15 +17,16 @@ type DriverConfig struct {
 }
 
 type Room struct {
-	ID      string              `json:"id"`
-	Devices map[DeviceID]Device `json:"devices"`
+	ID string `json:"id"`
+	// TODO proxy API requests to here
+	ProxyBaseURL string              `json:"-"`
+	Devices      map[DeviceID]Device `json:"devices"`
 }
 
 type Device struct {
-	Address string                    `json:"address"`
-	Driver  string                    `json:"driver"`
-	Proxy   map[*regexp.Regexp]string `json:"-"`
-	Ports   Ports                     `json:"ports,omitempty"`
+	Address string `json:"address"`
+	Driver  string `json:"driver"`
+	Ports   Ports  `json:"ports,omitempty"`
 }
 
 type Ports []Port
@@ -56,22 +55,4 @@ func (d Device) TypePorts(typ string) Ports {
 	}
 
 	return p
-}
-
-func (d Device) MarshalJSON() ([]byte, error) {
-	type Alias Device
-
-	changed := struct {
-		*Alias
-		Proxy map[string]string `json:"proxy,omitempty"`
-	}{
-		Alias: (*Alias)(&d),
-		Proxy: make(map[string]string),
-	}
-
-	for k, v := range d.Proxy {
-		changed.Proxy[k.String()] = v
-	}
-
-	return json.Marshal(changed)
 }
