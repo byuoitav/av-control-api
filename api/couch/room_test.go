bd28387133838405ce85a75403ac4e0206f19b81
Daniel Randall <dgrandall@me.com>
2020-06-29 14:48:48 -0600

    one more mapping test

--- api/couch/room_test.go
+++ api/couch/room_test.go
@@ -2,20 +2,20 @@ package couch
 
 import (
 	"testing"
-
-	"github.com/go-kivik/kivikmock/v3"
 )
 
 func TestRoom(t *testing.T) {
-	client, mock, err := kivikmock.New()
-	if err != nil {
-		t.Fatalf("unable to create kivik mock: %s", err)
-	}
+	/*
+		client, mock, err := kivikmock.New()
+		if err != nil {
+			t.Fatalf("unable to create kivik mock: %s", err)
+		}
 
-	ds := &DataService{
-		client:       client,
-		database:     _defaultDatabase,
-		mappingDocID: _defaultMappingDocID,
-		environment:  "default",
-	}
+		ds := &DataService{
+			client:       client,
+			database:     _defaultDatabase,
+			mappingDocID: _defaultMappingDocID,
+			environment:  "default",
+		}
+	*/
 }
