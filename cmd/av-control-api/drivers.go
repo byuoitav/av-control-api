574ca6e89a28765128b49055f5bb378c6995d146
Andrew Werner <klinc9@gmail.com>
2020-10-16 13:16:07 -0600

    still working on them driver boys

--- cmd/av-control-api/drivers.go
+++ cmd/av-control-api/drivers.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	avcontrol "github.com/byuoitav/av-control-api"
-	"github.com/byuoitav/av-control-api/drivers/adcp"
+	"github.com/byuoitav/av-control-api/drivers/core"
 )
 
 // TODO wrap new functions
 func registerDrivers(d avcontrol.DriverRegistry) {
-	d.MustRegister("sonyADCP", &adcp.SonyADCPDriver{})
+	d.MustRegister("sonyADCP", &core.SonyADCPDriver{})
+	// d.MustRegister("Atlona", &core.AtlonaDriver{})
+	d.MustRegister("JAP", &core.JAPDriver{})
+	// d.MustRegister("KeyDigital", &core.KeyDigitalDriver{})
+	d.MustRegister("kramer", &core.KramerDriver{})
+	// d.MustRegister("London", &core.LondonDriver{})
+	// d.MustRegister("NEC", &core.NECDriver{})
+	d.MustRegister("QSC", &core.QSCDriver{})
+	d.MustRegister("sony", &core.SonyDriver{})
 }
--- drivers/core/london.go
+++ drivers/core/london.go
@@ -1,19 +1,11 @@
 package core
 
-import (
-	"context"
-
-	avcontrol "github.com/byuoitav/av-control-api"
-	"github.com/byuoitav/av-control-api/drivers"
-	"github.com/byuoitav/london-driver"
-)
-
 type LondonDriver struct{}
 
 func (l *LondonDriver) ParseConfig(config map[string]interface{}) error {
 	return nil
 }
 
-func (l *LondonDriver) CreateDevice(ctx context.Context, addr string) (avcontrol.Device, error) {
-	return london.New(addr, london.WithLogger(drivers.Log.Named(addr))), nil
-}
+// func (l *LondonDriver) CreateDevice(ctx context.Context, addr string) (avcontrol.Device, error) {
+// 	return london.New(addr, london.WithLogger(drivers.Log.Named(addr))), nil
+// }
