c13b5bc8f0849bcaca45395022e6a95b10d45607
Brayden Winterton <bwinterton@gmail.com>
2020-10-19 22:45:44 -0600

    fix lock_table

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket     = "terraform-state-storage-586877430255"
-    lock_table = "terraform-state-lock-586877430255"
+    dynamodb_table = "terraform-state-lock-586877430255"
     region     = "us-west-2"
 
     // THIS MUST BE UNIQUE
