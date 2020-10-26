364f73678234275fa0b885cf94e61aab9f9a71b6
Andrew Werner <klinc9@gmail.com>
2020-10-26 13:55:38 -0600

    updating lots of drivers

--- cmd/av-control-api/drivers.go
+++ cmd/av-control-api/drivers.go
@@ -8,33 +8,52 @@ import (
 
 // TODO wrap new functions
 func registerDrivers(d avcontrol.DriverRegistry, log *zap.Logger) {
-	d.MustRegister("sonyADCP", &core.SonyADCPDriver{
-		Log: log.Named("drivers/sonyADCP"),
+	d.MustRegister("sony/commonrest", &core.SonyADCPDriver{
+		Log: log.Named("drivers/sony/commonrest"),
 	})
+
 	d.MustRegister("atlona6x2", &core.Atlona6x2Driver{})
+
 	d.MustRegister("atlona5x1", &core.Atlona5x1Driver{
 		Log: log.Named("drivers/atlona"),
 	})
+
 	d.MustRegister("atlona4x1", &core.Atlona4x1Driver{})
+
 	d.MustRegister("atlona2x1", &core.Atlona2x1Driver{})
-	d.MustRegister("JAP", &core.JAPDriver{})
+
+	d.MustRegister("JAP", &core.JAPDriver{
+		Log: log.Named("drivers/JAP"),
+	})
+
 	d.MustRegister("keyDigital", &core.KeyDigitalDriver{
 		Log: log.Named("drivers/keyDigital"),
 	})
+
 	d.MustRegister("kramerDSP", &core.KramerDSPDriver{
 		Log: log.Named("driver/kramerDSP"),
 	})
-	d.MustRegister("kramerVia", &core.KramerViaDriver{
+
+	d.MustRegister("kramer/via", &core.KramerViaDriver{
 		Log: log.Named("drivers/kramerVia"),
 	})
+
 	d.MustRegister("kramerVSDSP", &core.KramerVSDSPDriver{
 		Log: log.Named("driver/kramerVSDSP"),
 	})
+
 	d.MustRegister("kramer/protocol3000", &core.KramerProtocol3000Driver{})
+
 	d.MustRegister("london", &core.LondonDriver{
 		Log: log.Named("drivers/london"),
 	})
 	// d.MustRegister("NEC", &core.NECDriver{})
-	d.MustRegister("QSC", &core.QSCDriver{})
-	d.MustRegister("sony", &core.SonyDriver{})
+
+	d.MustRegister("QSC", &core.QSCDriver{
+		Log: log.Named("drivers/qsc"),
+	})
+
+	d.MustRegister("sony/bravia", &core.SonyDriver{
+		Log: log.Named("drivers/sony/bravia"),
+	})
 }
