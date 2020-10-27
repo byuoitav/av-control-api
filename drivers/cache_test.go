bb1c81a16ac1282bfd5bb342b72782b08e671710
Daniel Randall <dgrandall@me.com>
2020-10-27 13:19:33 -0600

    cache testing

--- drivers/cache_test.go
+++ drivers/cache_test.go
@@ -1,71 +1,85 @@
 package drivers
 
-/*
-// TODO this should test the deviceCache
+import (
+	"context"
+	"math/rand"
+	"testing"
+	"time"
+
+	avcontrol "github.com/byuoitav/av-control-api"
+	"github.com/matryer/is"
+	"golang.org/x/sync/errgroup"
+)
+
+type testDriver struct {
+	createDelay func() time.Duration
+}
+
+func (d *testDriver) ParseConfig(config map[string]interface{}) error {
+	return nil
+}
+
+func (d *testDriver) CreateDevice(ctx context.Context, addr string) (avcontrol.Device, error) {
+	time.Sleep(d.createDelay())
+	return &struct {
+		// makes sure that we don't return the same address every time
+		// https://golang.org/ref/spec#Size_and_alignment_guarantees
+		// > zero-size variables may have the same address
+		field string
+	}{}, nil
+}
+
 func TestSavingDevices(t *testing.T) {
+	is := is.New(t)
 	rand.Seed(time.Now().Unix())
 
-	newDev := saveDevicesFunc(func(ctx context.Context, addr string) (Device, error) {
-		time.Sleep(time.Duration(rand.Intn(50)) * time.Millisecond)
-		return &mock.TV{}, nil
-	})
+	cache := &deviceCache{
+		Driver: &testDriver{
+			createDelay: func() time.Duration {
+				return time.Duration(rand.Intn(50)) * time.Millisecond
+			},
+		},
+		cache: make(map[string]avcontrol.Device),
+	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	addr := "1.1.1.1"
-	dev, err := newDev(ctx, addr)
-	if err != nil {
-		t.Fatalf("unable to create device: %s", err)
-	}
+	dev, err := cache.CreateDevice(ctx, addr)
+	is.NoErr(err)
 
-	for i := 0; i < 20; i++ {
-		d, err := newDev(ctx, addr)
-		if err != nil {
-			t.Fatalf("unable to create device: %s", err)
-		}
-
-		if d != dev {
-			t.Fatalf("got mismatched devices: expected %p, got %p", dev, d)
-		}
+	for i := 0; i < 100; i++ {
+		d, err := cache.CreateDevice(ctx, addr)
+		is.NoErr(err)
+		is.True(d == dev)
 	}
 
 	// try a few random addresses, make sure they are different
-	d, err := newDev(ctx, "")
-	if err != nil {
-		t.Fatalf("unable to create device: %s", err)
-	}
+	d, err := cache.CreateDevice(ctx, "1.0.0.1")
+	is.NoErr(err)
+	is.True(d != dev)
 
-	if d == dev {
-		t.Fatalf("got the same devices for different addresses: got %p", d)
-	}
+	d, err = cache.CreateDevice(ctx, "0.0.0.0")
+	is.NoErr(err)
+	is.True(d != dev)
 
-	// try with 0.0.0.0
-	d, err = newDev(ctx, "0.0.0.0")
-	if err != nil {
-		t.Fatalf("unable to create device: %s", err)
-	}
-
-	if d == dev {
-		t.Fatalf("got the same devices for different addresses: got %p", d)
-	}
-
-	// try with one more
-	d, err = newDev(ctx, "this is a string!")
-	if err != nil {
-		t.Fatalf("unable to create device: %s", err)
-	}
-
-	if d == dev {
-		t.Fatalf("got the same devices for different addresses: got %p", d)
-	}
+	d, err = cache.CreateDevice(ctx, "random string")
+	is.NoErr(err)
+	is.True(d != dev)
 }
 
 func TestSaveDevicesAtSameTime(t *testing.T) {
-	newDev := saveDevicesFunc(func(ctx context.Context, addr string) (Device, error) {
-		time.Sleep(2 * time.Second)
-		return &mock.TV{}, nil
-	})
+	is := is.New(t)
+
+	cache := &deviceCache{
+		Driver: &testDriver{
+			createDelay: func() time.Duration {
+				return 2 * time.Second
+			},
+		},
+		cache: make(map[string]avcontrol.Device),
+	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -73,64 +87,46 @@ func TestSaveDevicesAtSameTime(t *testing.T) {
 	group, gctx := errgroup.WithContext(ctx)
 	addr := "1.1.1.1"
 
-	devs := make([]Device, 3)
+	devs := make([]avcontrol.Device, 3)
 	done := make([]time.Time, 3)
 
 	group.Go(func() error {
 		var err error
-		devs[0], err = newDev(gctx, addr)
-		if err != nil {
-			return err
-		}
-
+		devs[0], err = cache.CreateDevice(gctx, addr)
 		done[0] = time.Now()
-		return nil
+		return err
 	})
 
 	group.Go(func() error {
 		time.Sleep(50 * time.Millisecond)
 
 		var err error
-		devs[1], err = newDev(gctx, addr)
-		if err != nil {
-			return err
-		}
-
+		devs[1], err = cache.CreateDevice(gctx, addr)
 		done[1] = time.Now()
-		return nil
+		return err
 	})
 
 	group.Go(func() error {
 		time.Sleep(100 * time.Millisecond)
 
 		var err error
-		devs[2], err = newDev(gctx, addr)
-		if err != nil {
-			return err
-		}
-
+		devs[2], err = cache.CreateDevice(gctx, addr)
 		done[2] = time.Now()
-		return nil
+		return err
 	})
 
-	err := group.Wait()
-	if err != nil {
-		t.Fatalf("unexpected error creating devices: %s", err)
-	}
+	is.NoErr(group.Wait())
 
 	// make sure all the devs are the same
-	if devs[0] != devs[1] || devs[0] != devs[2] {
-		t.Fatalf("not all devices matched. got %p, %p, and %p", devs[0], devs[1], devs[2])
+	for i := 0; i < len(devs)-2; i++ {
+		is.True(devs[i] == devs[i+1])
 	}
 
-	// make sure they all showed up at about the same time
-	rounded := make([]time.Time, 3)
-	rounded[0] = done[0].Round(25 * time.Millisecond)
-	rounded[1] = done[1].Round(25 * time.Millisecond)
-	rounded[2] = done[2].Round(25 * time.Millisecond)
+	// make sure they showed up at about the same time
+	for i := 0; i < len(done)-2; i++ {
+		cur := done[i].Round(25 * time.Millisecond)
+		next := done[i].Round(25 * time.Millisecond)
 
-	if !rounded[0].Equal(rounded[1]) || !rounded[0].Equal(rounded[2]) {
-		t.Fatalf("didn't finish around the same time. finished at %v, %v, and %v", done[0].Format(time.RFC3339Nano), done[1].Format(time.RFC3339Nano), done[2].Format(time.RFC3339Nano))
+		is.Equal(cur, next)
 	}
 }
-*/
