efe3123002a219a9760cf99e19dc739405fd0f80
Andrew Werner <klinc9@gmail.com>
2020-11-03 10:15:49 -0700

    lil bit o testing for couchy boi

--- couch/couch_test.go
+++ couch/couch_test.go
@@ -1,14 +1,26 @@
 package couch
 
+import (
+	"context"
+	"testing"
+
+	"github.com/matryer/is"
+)
+
 // I'm not even sure this is possible...
-/*
-func TestAuth(t *testing.T) {
+// func TestAuth(t *testing.T) {
+// 	is := is.New(t)
+
+// 	client, _, err := kivikmock.New()
+// 	is.NoErr(err)
+
+// 	_, err = NewWithClient(context.Background(), client)
+// 	is.NoErr(err)
+// }
+
+func TestNew(t *testing.T) {
 	is := is.New(t)
 
-	client, mock, err := kivikmock.New()
-	is.NoErr(err)
-
-	ds, err := NewWithClient(context.Background(), client)
-	is.NoErr(err)
+	_, err := New(context.TODO(), "")
+	is.Equal("unable to build client: no URL specified", err.Error())
 }
-*/
